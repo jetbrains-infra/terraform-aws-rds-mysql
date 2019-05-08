ec3daa14c5c9ae5f6c01df77480d36fdaba6e9ed
Sergei Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2019-05-08 10:41:47 +0200

    fix: code formatting

--- security_groups.tf
+++ security_groups.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "default" {
   vpc_id = "${local.vpc_id}"
 
   ingress {
-    from_port = 3306
-    protocol = "TCP"
-    to_port = 3306
+    from_port   = 3306
+    protocol    = "TCP"
+    to_port     = 3306
     cidr_blocks = ["${var.trusted_cidr_blocks}"]
   }
 
